703fe51cbb4aa0e3c8e7795236805ee98155e122
NazarK0 <staticman999@gmail.com>
2021-07-24 05:51:55 +0300

    2

--- server/src/controllers/googleController.js
+++ server/src/controllers/googleController.js
@@ -1,46 +1,60 @@
 const { client } = require('../google');
+const { google } = require('googleapis');
+const path = require('path');
 
 function listMajors(auth) {
-  const sheets = google.sheets('v4');
-  sheets.spreadsheets.values.get(
-    {
-      auth: auth,
-      spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-      range: 'Class Data!A2:E',
-    },
-    (err, res) => {
-      if (err) {
-        console.error('The API returned an error.');
-        throw err;
-      }
-      const rows = res.data.values;
-      if (rows.length === 0) {
-        console.log('No data found.');
-      } else {
-        console.log('Name, Major:');
-        for (const row of rows) {
-          // Print columns A and E, which correspond to indices 0 and 4.
-          console.log(`${row[0]}, ${row[4]}`);
+    const sheets = google.sheets('v4');
+    sheets.spreadsheets.values.get({
+            auth: auth,
+            spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+            range: 'Class Data!A2:E',
+        },
+        (err, res) => {
+            if (err) {
+                console.error('The API returned an error.');
+                throw err;
+            }
+            const rows = res.data.values;
+            if (rows.length === 0) {
+                console.log('No data found.');
+            } else {
+                console.log('Name, Major:');
+                for (const row of rows) {
+                    // Print columns A and E, which correspond to indices 0 and 4.
+                    console.log(`${row[0]}, ${row[4]}`);
+                }
+            }
         }
-      }
-    }
-  );
+    );
 }
 
-const getOauthCallback = (req, res) => {
-  const code = req.query.code;
-  client.getToken(code, (err, tokens) => {
-    if (err) {
-      console.error('Error getting oAuth tokens:');
-      throw err;
-    }
-    client.credentials = tokens;
-    res.send('Authentication successful! Please return to the console.');
-    server.close();
-    listMajors(client);
-  });
+const getOauthCallback = async(req, res) => {
+    // const code = req.query.code;
+    // client.getToken(code, (err, tokens) => {
+    //   if (err) {
+    //     console.error('Error getting oAuth tokens:');
+    //     throw err;
+    //   }
+    //   client.credentials = tokens;
+    //   res.send('Authentication successful! Please return to the console.');
+    //   server.close();
+    //   listMajors(client);
+    // });
+    const scopes = ['https://www.googleapis.com/auth/spreadsheets'];
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: path.join(__dirname, '../../..', 'credentials2.json'),
+        scopes,
+    });
+
+    const client = await auth.getClient();
+    const googleSheets = google.sheets({ version: 'v4', auth: client });
+
+    const metadata = await googleSheets.spreadsheets
+
+
 };
 
 module.exports = {
-  getOauthCallback,
+    getOauthCallback,
 }
--- server/src/routes/googleRoutes.js
+++ server/src/routes/googleRoutes.js
@@ -1,6 +1,23 @@
 const router = require("express").Router();
 const controller = require("../controllers/googleController");
 
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// passport.use(new GoogleStrategy({
+//         clientID: GOOGLE_CLIENT_ID,
+//         clientSecret: GOOGLE_CLIENT_SECRET,
+//         callbackURL: "http://www.example.com/auth/google/callback"
+//     },
+//     function(accessToken, refreshToken, profile, cb) {
+//         console.log(accessToken, 'accesToken')
+//         console.log(refreshToken, 'refreshToken')
+//         console.log(profile, 'profile')
+
+//     }
+// ));
+
 router.get('/oauth2callback', controller.getOauthCallback);
+
 
 module.exports = router;
